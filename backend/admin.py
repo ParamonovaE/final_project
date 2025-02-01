707c68790f3b95ee427b213145e45c9127965394
Kate Paramonova <katerinka777happy@mail.ru>
2025-02-01 19:56:52 +0300

    Добавлены некоторые модели и зарегистрировала их в admin.py

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Shop, Product, Category, ProductInfo, Parameter, ProductParameter
 
 # Register your models here.
+admin.site.register(Shop)
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(ProductInfo)
+admin.site.register(Parameter)
+admin.site.register(ProductParameter)
--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Shop(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name="Название магазина")
+    url = models.URLField(max_length=300, unique=True, help_text="Введите полный URL, включая https://")
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name="Название категории")
+    shops = models.ManyToManyField(Shop, related_name='categories')
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name="Название продукта")
+    categories = models.ManyToManyField(Category, related_name='products')
+
+class ProductInfo(models.Model):
+    product = models.ForeignKey(Product, related_name="product_infos", on_delete=models.CASCADE, verbose_name="Название продукта")
+    shop = models.ForeignKey(Shop, related_name="shop_infos", on_delete=models.CASCADE, verbose_name="Название магазина")
+    quantity = models.PositiveIntegerField(default=0, verbose_name="Количество")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
+    price_rrc = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Рекомендуемая цена")
+
+class Parameter(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name='Название параметра')
+
+class ProductParameter(models.Model):
+    product_info = models.ForeignKey(ProductInfo, related_name="product_infos", on_delete=models.CASCADE, verbose_name='Информация о продукте')
+    parameter = models.ForeignKey(Parameter, related_name="product_parameters", on_delete=models.CASCADE, verbose_name="Параметр")
+    value = models.CharField(max_length=255, verbose_name="Значение параметра")
+
